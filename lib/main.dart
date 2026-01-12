873defba49e4e1a9d8bb9b53502b0b88e270eebb
sellwia <Sellydwikurnia@gmail.com>
2026-01-12 10:09:42 +0700

    buat judul halaman apk

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
        debugShowCheckedModeBanner: false,
+       title: 'Miracle Of Gaming',
     );
   }
 }
