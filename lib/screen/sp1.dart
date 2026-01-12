b499870ad45a56f591c2edc26b20fb46e3ad90dd
sellwia <Sellydwikurnia@gmail.com>
2026-01-12 11:12:15 +0700

    title : buat font jadi bold

--- lib/screen/sp1.dart
+++ lib/screen/sp1.dart
@@ -39,7 +39,7 @@ class Screen1 extends StatelessWidget {
             Text(
               "Welcome To \n Miracle Of Gaming",
               textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 30, color: Colors.white, height: 1.2,),
+              style: TextStyle(fontSize: 30, color: Colors.white, height: 1.2, fontWeight: FontWeight.bold),
             ),
 
             SizedBox(height: 20),
