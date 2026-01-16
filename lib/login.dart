e3e32fe6137431ed633453213cc7d2eae90806a8
sellwia <Sellydwikurnia@gmail.com>
2026-01-16 22:43:27 +0700

    icon mata

--- lib/login.dart
+++ lib/login.dart
@@ -127,6 +127,7 @@ class LoginPage extends StatelessWidget {
                         Icons.lock_outline,
                         color: Colors.white,
                       ),
+                      suffixIcon: const Icon(Icons.visibility_outlined),
                     ),
                   ),
 
