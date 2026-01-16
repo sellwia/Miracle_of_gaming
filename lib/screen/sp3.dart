a4c863a7d4edbbf279e584f525af13a6d56d6140
sellwia <Sellydwikurnia@gmail.com>
2026-01-16 08:16:50 +0700

    update : ganti foto

--- lib/screen/sp3.dart
+++ lib/screen/sp3.dart
@@ -29,7 +29,7 @@ class Screen3 extends StatelessWidget {
                 shape: BoxShape.circle,
                 color: Colors.amber,
                 image: DecorationImage(
-                  image: AssetImage("assets/images/Game On!.jpg"),
+                  image: AssetImage("assets/images/Play.jpg"),
                 ),
               ),
             ),         
