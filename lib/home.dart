3e36dcd6195b52e9ef0366d7a7b3246cf114bab6
sellwia <Sellydwikurnia@gmail.com>
2026-01-19 22:56:58 +0700

    buat boxdecoration dengan gradasi warna

--- lib/home.dart
+++ lib/home.dart
@@ -30,6 +30,14 @@ class HomePage extends StatelessWidget {
       body: Container(
         width: double.infinity,
         height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF0F0F0F), Color(0xFF1C1F26), Color(0xFF2A2D34)],
+          ),
+        ),
+
         
       ),
     ); 
