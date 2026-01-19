846682caa086b955e54d3a89d991393e1901aab0
sellwia <Sellydwikurnia@gmail.com>
2026-01-19 22:39:56 +0700

    buat stl untuk poster dihalaman home

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+class PosterItem extends StatelessWidget {
+  const PosterItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -8,3 +17,4 @@ class HomePage extends StatelessWidget {
     return const Placeholder();
   }
 }
+
